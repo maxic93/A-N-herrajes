84bd98a2829fa9f56b6fe0c5afa38e0e9d153a26
Maximiliano Cabral <maxicabral36@gmail.com>
2022-04-04 13:54:01 -0300

    Primera parte de la pagina

---
+++ main.js
@@ -0,0 +1,10 @@
+//Variables
+const btnNab = document.querySelector(".fa-bars")
+const nav = document.querySelector(".container_nav")
+
+
+//Eventos
+btnNab.addEventListener("click", ()=> {
+    nav.classList.toggle("show_nav")
+})
+    
